3ee6d31a17b8a9706b444c61e28dddd0029063e2
Nick <nsantander1095@gmail.com>
2022-08-18 15:01:47 -0400

    wrote script.js based on demo in class, will refine later to make sure it all works'

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var lower = 'abcdefghijklmonpqrstuvwxyz';
+  var upper = lower.toUpperCase();
+  var numbers = '0123456789';
+  var special = '"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+
+  var characters = 12;
+
+  var hasLower = confirm('has lower case letter?');
+  var hasUpper = confirm('has upper case letter?');
+  var hasNumber = confirm('has number?');
+  var hasSpecial = confirm('has special character?');
+
+  var possible;
+
+  if (hasLower) {
+    possible += lower;
+  }
+  if (hasUpper) {
+    possible += upper;
+  }
+  if (hasNumber) {
+    possible += numbers;
+  }
+  if (hasSpecial) {
+    possible += special;
+  }
+
+  
+  debugger;
+
+  return '';
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  for (var i = 0; i < characters; i++) {
+    var random = Math.floor(Math.random() * possible.length);
+    console.log(random);
+    password += password[random];
+  }
+  
 
 }
 
